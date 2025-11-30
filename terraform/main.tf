cd1a41fb15c513009a9df09f7a7d4c07f60f5f93
delzio <jdelzio@gmail.com>
2025-11-30 01:08:09 -0800

    adding pubsub to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,3 +58,26 @@ resource "google_storage_bucket" "sample-bucket" {
     }
   }
 }
+
+resource "google_pubsub_topic" "chrom-stream-topic" {
+  name = "chrom-sensor-readings"
+
+  message_retention_duration = "86400s" # 24 hours
+}
+
+resource "google_pubsub_subscription" "chrom-stream-sub" {
+  name  = "chrom-sensor-readings-sub"
+  topic = google_pubsub_topic.chrom-stream-topic.name
+
+  # Ensure order for consumer integration calculations
+  enable_message_ordering = true
+
+  ack_deadline_seconds       = 10
+  message_retention_duration = "86400s"
+
+  # pull-mode
+  expiration_policy {
+    ttl = "604800s" # 7 days
+  }
+}
+
