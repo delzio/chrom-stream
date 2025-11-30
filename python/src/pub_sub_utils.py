842ef7afca159a15065a566c75968cdebce958e6
delzio <jdelzio@gmail.com>
2025-11-30 01:07:50 -0800

    adding totalized volume integrations to trend streaming

--- python/src/pub_sub_utils.py
+++ python/src/pub_sub_utils.py
@@ -7,17 +7,22 @@ CREDENTIALS = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 PROJECT_ID = os.environ["GCP_PROJECT_ID"]
 TOPIC_ID = "chrom-sensor-readings"   # name of topic
 SUBSCRIPTION_ID = os.environ["PUBSUB_SUBSCRIPTION_ID"]
-publisher = pubsub_v1.PublisherClient()
+publisher = pubsub_v1.PublisherClient(
+    publisher_options=pubsub_v1.types.PublisherOptions(enable_message_ordering=True)
+)
 
 def publish(message: dict) -> None:
     """ Publish message formatted as dictionary to GCP Pub/Sub """
 
     topic_path = publisher.topic_path(PROJECT_ID, TOPIC_ID)
 
+    ordering_key = message.get("chrom_unit", "default")
+
     future = publisher.publish(
         topic_path,
         json.dumps(message).encode("utf-8"),
-        source="python_test"
+        source="python_time_series_trend_generator",
+        ordering_key=ordering_key
     )
 
     print(f"publishing data to pubsub: {message}")
--- python/src/stream_consumer/main.py
+++ python/src/stream_consumer/main.py
@@ -6,39 +6,92 @@ from influxdb_client_3 import InfluxDBClient3, Point
 
 from pub_sub_utils import subscribe
 
-# INFLUXDB
+# INFLUXDB CONFIG
 TOKEN = os.environ["INFLUXDB_WRITE_TOKEN"]
 ORG = "Dev"
 HOST = "https://us-east-1-1.aws.cloud2.influxdata.com"
 DATABASE = os.environ["INFLUXDB_BUCKET"]
 client = InfluxDBClient3(host=HOST, token=TOKEN, org=ORG)
 
+# INITIALIZE GLOBALS
+buffer = {}
+last_timestamp_ns = {}
+last_flow_rate = {}
+totalized_volume_ml = {}
+
+def handle_event(event: dict) -> None:
+    """ Process individual event from Pub/Sub """
+    global last_timestamp_ns, last_flow_rate, totalized_volume_ml
+
+    # Calculate totalized volume
+    chrom_unit = event["chrom_unit"]
+    cur_ts = int(event["time_ns"])
+    cur_flow = event["flow_mL_min"]
+
+    last_ts = last_timestamp_ns.get(chrom_unit, None)
+    last_flow = last_flow_rate.get(chrom_unit, 0)
+    if chrom_unit not in totalized_volume_ml:
+        totalized_volume_ml[chrom_unit] = 0.0
+
+    if last_ts is not None and cur_ts < last_ts:
+        print(f"Skipping out of order event for {chrom_unit}. Current TS: {cur_ts}, Last TS: {last_ts}")
+        return
+
+    if last_ts is not None:
+        # Calculate time difference in minutes
+        delta_min = (cur_ts - last_ts) / 1e9 / 60.0
+        # Average flow rate between last and current
+        avg_flow = (last_flow + cur_flow) / 2.0
+        # Calculate volume added since last event
+        delta_vol = avg_flow * delta_min
+        totalized_volume_ml[chrom_unit] += delta_vol
+
+    # Calculate totalized column volumes (assuming 226 L column volume)
+    tot_col_vol = totalized_volume_ml[chrom_unit] / 1000.0 / 226 
+
+    last_timestamp_ns[chrom_unit] = cur_ts
+    last_flow_rate[chrom_unit] = cur_flow
+
+    point = (
+        Point("chromatography")
+        .tag("instrument", event["chrom_unit"])
+        .field("uv_mau", event["uv_mau"])
+        .field("cond_mScm", event["cond_mScm"])
+        .field("ph", event["ph"])
+        .field("flow_mL_min", event["flow_mL_min"])
+        .field("pressure_bar", event["pressure_bar"])
+        .field("totalized_volume_ml", totalized_volume_ml[chrom_unit])
+        .field("totalized_column_volumes", tot_col_vol)
+        .time(cur_ts) 
+    )
+    client.write(database=DATABASE, record=point)
+    print("data point submitted to influxdb")
+
 
 def process_message(message: pubsub_v1.subscriber.message.Message) -> None:
     """ Triggered automatically whenever a Pub/Sub message arrives """
     try:
         data = json.loads(message.data.decode("utf-8"))
-        print("Received message:", data)
-        
-        point = (
-            Point("chromatography")
-            .tag("instrument", data["chrom_unit"])
-            .field("uv_mau", data["uv_mau"])
-            .field("cond_mScm", data["cond_mScm"])
-            .field("ph", data["ph"])
-            .field("flow_mL_min", data["flow_mL_min"])
-            .field("pressure_bar", data["pressure_bar"])
-            .time(data["time_ns"]) 
-        )
+        chrom_unit = data["chrom_unit"]
 
-        client.write(database=DATABASE, record=point)
-        print("data point submitted to influxdb")
+        # buffer data by chrom unit
+        if chrom_unit not in buffer:
+            buffer[chrom_unit] = []
+        else:
+            buffer[chrom_unit].append(data)
+        
+        # sort buffer by timestamp
+        buffer[chrom_unit] = sorted(buffer[chrom_unit], key=lambda x: int(x["time_ns"]))
+
+        # ensure buffered events are handled in order
+        for event in buffer[chrom_unit]:
+            if last_timestamp_ns.get(chrom_unit, 0) < int(event["time_ns"]):
+                handle_event(event)
+        
         message.ack()
     except Exception as e:
-        print(f"Error in submission to influxdb: {e}")
-    #except json.decoder.JSONDecodeError:
-    #    print(f"Message not formatted correctly, acknowledging and ignoring: {message.data}")
-    #    message.ack()
+        print(f"Error collecting data from pub/sub: {e}")
+
 
 def main():
     subscribe(callback_fn=process_message)
